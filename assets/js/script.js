be98066a69969466abf07df724d2856660e3a880
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-11 17:05:43 +0200

    Exercice 10 corriger sur le passage de la souris et des changement de CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
+// Au passage de la souris (évenement mouseover) sur mon élément qui à l'id (#green, #red ou #blue), je modifie le css de mon élément id #text
 $(function(){
-    $('#green').mouseover(function(){
+  $('.color').mouseover(function(){
+    // Je stock dans la variable buttonColor la valeur de l'attribue Id de l'élément sur lequel je suis.
+     buttonColor = $(this).attr('id');
+     consol.log(buttonColor);
+     $('#text').css('color', buttonColor)
+
+  });
+    /*$('#green').mouseover(function(){
       $('#text').css('color', 'green');
-    });
-    $('#green').mouseout(function(){
-      $('#text').css('color', 'black');
     });
     $('#red').mouseover(function(){
       $('#text').css('color', 'red');
     });
-    $('#red').mouseout(function(){
-      $('#text').css('color', 'black');
-    });
     $('#blue').mouseover(function(){
       $('#text').css('color', 'blue');
     });
-    $('#blue').mouseout(function(){
+    */
+    // Je cible tous les éléments qui ont la classe '.color', au moment où ma souris quitte ces éléments, je modifie les éléments de mon id '#text'
+    $('.color').mouseout(function(){
       $('#text').css('color', 'black');
     });
-
 })
