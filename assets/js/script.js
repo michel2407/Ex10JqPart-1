00a4f748b01235d056caa7cf05269f0614bc87a7
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-11 15:06:53 +0200

    Exercice 10 avant correction

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('#green').mouseover(function(){
+      $('#text').css('color', 'green');
+    });
+    $('#green').mouseout(function(){
+      $('#text').css('color', 'black');
+    });
+    $('#red').mouseover(function(){
+      $('#text').css('color', 'red');
+    });
+    $('#red').mouseout(function(){
+      $('#text').css('color', 'black');
+    });
+    $('#blue').mouseover(function(){
+      $('#text').css('color', 'blue');
+    });
+    $('#blue').mouseout(function(){
+      $('#text').css('color', 'black');
+    });
+
+})
